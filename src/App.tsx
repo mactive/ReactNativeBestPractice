22ad8273ee71515444a1905d759381213a664273
mactive <mactive@gmail.com>
2019-08-10 17:20:42 +0800

    use styled component and vscode plugin

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { createAppContainer, createStackNavigator } from "react-navigation";
 import { MainScreen } from "./MainScreen";
 import { FindFoodScreen } from "./pages/FindFood";
 import { TheoryRenderScreen } from "./pages/RNTheory/TheoryRenderScreen";
+import { CardScreen } from "./pages/AdvanceGesture/CardScreen.tsx";
 
 const mainNavigator = createStackNavigator({
+	CardScreen: { screen: CardScreen },
 	TheoryRender: { screen: TheoryRenderScreen },
 	Main: { screen: MainScreen },
 	FindFood: { screen: FindFoodScreen },
