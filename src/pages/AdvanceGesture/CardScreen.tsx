32c65355250776e5236a48549713180d35b2e0ee
mactive <mactive@gmail.com>
2019-08-20 22:21:44 +0800

    add assets

--- src/pages/AdvanceGesture/CardScreen.tsx
+++ src/pages/AdvanceGesture/CardScreen.tsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native';
-import styled from "styled-components";
+import { Text, View, PanResponder, Animated, PanResponderStatic } from 'react-native';
+import styled from "styled-components/native";
+import Card from './Card'
+interface Props {
+    name?: string
+}
+export class CardScreen extends Component<Props> {
 
-export class CardScreen extends Component {
+    public panResponder!: PanResponderStatic;
+
+    constructor(props: Props) {
+        super(props);
+    } 
+
+    state = {
+        pan: new Animated.ValueXY()
+    }
+
+    componentWillMount() {
+        this.panResponder = PanResponder.create({
+            onMoveShouldSetPanResponder: () => true
+        })
+    }
+
     public render() {
         return (
             <Container>
-                <Text>CardScreen</Text>
+                <Card
+                    title="Price Tag"
+                    image={require("../assets/background5.jpg")}
+                    author="Liu Yi"
+                    text="Thanks to Design+Code, I improved my design skill and learned to do animations for my app Price Tag, a top news app in China."
+                />
             </Container>
         )
     }
