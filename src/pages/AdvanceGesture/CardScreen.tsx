8cb51ec0c23aa4cada15f266282fd81d09a986d1
mactive <mactive@gmail.com>
2019-08-20 22:55:08 +0800

    增加基础手势

--- src/pages/AdvanceGesture/CardScreen.tsx
+++ src/pages/AdvanceGesture/CardScreen.tsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
-import { Text, View, PanResponder, Animated, PanResponderStatic } from 'react-native';
+import { PanResponder, Animated, PanResponderInstance } from 'react-native';
 import styled from "styled-components/native";
 import Card from './Card'
 interface Props {
     name?: string
 }
-export class CardScreen extends Component<Props> {
+interface State {
+    pan: Animated.ValueXY
+}
+export class CardScreen extends Component<Props, State> {
 
-    public panResponder!: PanResponderStatic;
+    public panResponder!: PanResponderInstance;
 
     constructor(props: Props) {
         super(props);
@@ -19,19 +22,39 @@ export class CardScreen extends Component<Props> {
 
     componentWillMount() {
         this.panResponder = PanResponder.create({
-            onMoveShouldSetPanResponder: () => true
+            onMoveShouldSetPanResponder: () => true,
+            // onPanResponderGrant: (e,gestureState) => {},
+            onPanResponderMove: Animated.event([
+                null,
+                { dx: this.state.pan.x, dy: this.state.pan.y }
+            ]),
+            onPanResponderRelease: () => {
+                Animated.spring(this.state.pan, { 
+                    toValue: { x: 0, y: 0}
+                }).start()
+            },
         })
     }
 
     public render() {
         return (
             <Container>
+                <Animated.View
+                    style={{
+                        transform: [
+                            { translateX: this.state.pan.x },
+                            { translateY: this.state.pan.y },
+                        ]
+                    }}
+                    {...this.panResponder.panHandlers}
+                >
                 <Card
                     title="Price Tag"
                     image={require("../../assets/background5.jpg")}
                     author="Liu Yi"
                     text="Thanks to Design+Code, I improved my design skill and learned to do animations for my app Price Tag, a top news app in China."
                 />
+                </Animated.View>
             </Container>
         )
     }
